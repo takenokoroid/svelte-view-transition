f8bb2fa35e16f737a3e688340448cea43d6ca88e
takenokoroid <takenokoroid@gmail.com>
2023-03-16 15:31:38 +0000

    feat: 既存のカウンターと比較するために新たに作成

---
+++ src/routes/pageTransitonCounter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { pageTransition } from '$lib/transition';
+
+	let count = 0;
+	const increment = () => {
+		count++;
+	};
+</script>
+
+<div class="content">{count}</div>
+<button on:click={() => pageTransition(increment)}>increment</button>
+
+<style>
+	.content {
+		font-size: 36pt;
+	}
+</style>
